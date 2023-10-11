89a6118a01052b8073d064b6433695f78e99be44
Crowdbotics <engineering@crowdbotics.com>
2023-10-11 06:35:15 +0000

    Initial commit

---
+++ backend/proud_limit_dev_121645/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for proud_limit_dev_121645 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "proud_limit_dev_121645.settings"
+)
+
+application = get_wsgi_application()
